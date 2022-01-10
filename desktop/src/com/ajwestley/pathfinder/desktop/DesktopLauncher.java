ac1cc5d12a6fbc02c4eb52112735abc2f0ee5da9
AJ Westley <alexanderjwestley@gmail.com>
2022-01-10 14:45:27 +0200

    first version

---
+++ desktop/src/com/ajwestley/pathfinder/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.ajwestley.pathfinder.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.ajwestley.pathfinder.Pathfinder;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Pathfinder(), config);
+	}
+}
